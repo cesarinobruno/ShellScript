d6f96d9facfdcc1cd5d8ea78ef89be2bbe42fe1d
Bruno Cesarino <44678706+cesarinobruno@users.noreply.github.com>
2024-08-20 12:11:06 -0300

    Update reload-vpn.sh
    No final da rotina de reconexão, desativar e ativar a placa de rede para evitar erros de ponteiro de memória

--- reload-vpn.sh
+++ reload-vpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #VPN BB
-#210 minutos -> 3 horas e 30 minutos
+#238 minutos -> 3 horas e 58 minutos
 #printf "xxx" | snx -> preencher a senha e executar comando
 #snx -d -> desconectar a vpn
 
@@ -10,13 +10,16 @@ setup_vpn() {
    printf "senha" | snx
    echo "VPN rodando..."
    
-   sleep 210m
+   printf -v hoursAndMinutes "%(%H:%M)T\n"
+   echo "$hoursAndMinutes"
+   
+   sleep 238m
    snx -d	
 }
 
 x=0
 
-while [[ $x -le 3 ]]
+while [[ $x -le 4 ]]
 do 
    echo "Rotina iniciada..."
    sleep 1s
@@ -24,13 +27,17 @@ do
  
    setup_vpn
  
-   if [[ $x -eq 2 ]] 
+   if [[ $x -eq 3 ]] 
       then
+      	echo "Reiniciando adaptador Wifi"
+      	echo "senha" | sudo -S nmcli device disconnect wlp0s20f3
+      	sleep 5s
+      	sudo nmcli device connect wlp0s20f3
+        sleep 5s
         echo "Finalizado"
       break
    fi
    
 done
-
 
 
